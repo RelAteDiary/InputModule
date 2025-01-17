4904a339cdf4df389df3e42b2cb3209c4cd5add1
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-01-17 04:39:49 +0000

    Created ServerModule1, emailModule and LICENSE.txt; Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,5 +1,9 @@
 from ._anvil_designer import Form1Template
 from anvil import *
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
 
 
 class Form1(Form1Template):
@@ -8,3 +12,20 @@ class Form1(Form1Template):
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
+
+  def clear_inputs(self):
+    # Clear our three text boxes
+    self.name_box.text = ""
+    self.email_box.text = ""
+    self.feedback_box.text = ""
+    
+  def submit_button_click(self, **event_args):
+    """This method is called when the button is clicked"""
+    name = self.name_box.text
+    email = self.email_box.text
+    feedback = self.feedback_box.text
+
+    anvil.server.call('send_feedback',name,email,feedback)
+
+    Notification("Feedback submitted!").show()
+    self.clear_inputs()
