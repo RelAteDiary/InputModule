e5fd7094b5f5e3ae06897dbea97d349cd9648e47
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-02-09 06:04:10 +0000

    Created InTake.IntakeLanding and InTake.Question1; Edited settings, InTake, standard-page.html and theme/parameters.yaml; Deleted InTake

--- client_code/InTake/__init__.py
+++ client_code/InTake/__init__.py
@@ -1,34 +1,18 @@
-from ._anvil_designer import InTakeTemplate
-from anvil import *
+import anvil.server
 import anvil.google.auth, anvil.google.drive
 from anvil.google.drive import app_files
 import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.server
+# This is a package.
+# You can define variables and functions here, and use them from any form. For example, in a top-level form:
+#
+#    from .. import Package1
+#
+#    Package1.say_hello()
+#
 
 
-class InTake(InTakeTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
-    self.init_components(**properties)
-
-    # Any code you write here will run before the form opens.
-
-  def clear_inputs(self):
-    # Clear our three text boxes
-    self.name_box.text = ""
-    self.email_box.text = ""
-    self.feedback_box.text = ""
-    
-  def submit_button_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    name = self.name_box.text
-    email = self.email_box.text
-    feedback = self.feedback_box.text
-
-    anvil.server.call('send_feedback',name,email,feedback)
-
-    Notification("Feedback submitted!").show()
-    self.clear_inputs()
+def say_hello():
+  print("Hello, world")
