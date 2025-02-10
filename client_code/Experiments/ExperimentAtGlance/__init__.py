fa5c14422580d04624275d45a236e4b63ef84015
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-02-10 22:23:01 +0000

    Created serverExperiments; Edited Experiments.ExperimentAtGlance; Deleted ServerModule1

--- client_code/Experiments/ExperimentAtGlance/__init__.py
+++ client_code/Experiments/ExperimentAtGlance/__init__.py
@@ -7,7 +7,7 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-from ... import Experiments
+
 
 class ExperimentAtGlance(ExperimentAtGlanceTemplate):
   def __init__(self, **properties):
@@ -15,7 +15,7 @@ class ExperimentAtGlance(ExperimentAtGlanceTemplate):
     self.init_components(**properties)
 
     gp = GridPanel()
-    gp.add_component(Label(text=Experiments.get_experiment_type_text("""TODO""")),
+    gp.add_component(Label(text=anvil.server.call.get_experiment_type_text("""TODO""")),
                  row="title", col_xs=0, width_xs=2)
     gp.add_component(Label(text=Experiments.get_experiment_parameter_text("""TODO""")),
                  row="content", col_xs=0, width_xs=2)
---
+++ server_code/serverExperiments.py
@@ -0,0 +1,44 @@
+import anvil.secrets
+import anvil.google.auth, anvil.google.drive, anvil.google.mail
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.email
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+import anvil.server
+
+# This is a server module. It runs on the Anvil server,
+# rather than in the user's browser.
+#
+# To allow anvil.server.call() to call functions here, we mark
+# them with @anvil.server.callable.
+# Here is an example - you can replace it with your own:
+#
+# @anvil.server.callable
+# def say_hello(name):
+#   print("Hello, " + name + "!")
+#   return 42
+#
+liu = app_tables.users.get(email='lutetium.helen@gmail.com')
+app_tables.experiments.add_row(Person=liu,
+                             Text="Jane is a good kid")
+
+@anvil.server.callable
+def experiments_get_current():
+  app_tables.experiments.search(
+    user=anvil.users.get_user(),
+    end=q.any_of(None, q.greater_than(date.today())))
+  
+@anvil.server.callable
+def experiments_get_type_text(db_type):
+  if db_type == constants.EXPERIMENT_ELIMINATION:
+    return 'Elimination diet'
+  elif db_type == constants.EXPERIMENT_REINTRODUCTION:
+    return 'Reintroduction diet'
+  elif db_type == constants.EXPERIMENT_SELF_DIRECTED:
+    return 'Experiment'
+
+@anvil.server.callable
+def experiments_get_parameter_text(db_text):
+  return db_text
