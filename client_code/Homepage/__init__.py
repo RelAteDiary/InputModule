6492e8632c36165e75334fbd54aa260cde5cd60f
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-02-10 05:17:08 +0000

    Created Homepage and Homepage.RowTemplate1; Edited InTake.IntakeQuestions

--- client_code/Homepage/__init__.py
+++ client_code/Homepage/__init__.py
@@ -1,4 +1,4 @@
-from ._anvil_designer import IntakeQuestionsTemplate
+from ._anvil_designer import HomepageTemplate
 from anvil import *
 import anvil.server
 import anvil.google.auth, anvil.google.drive
@@ -9,19 +9,13 @@ import anvil.tables.query as q
 from anvil.tables import app_tables
 
 
-class IntakeQuestions(IntakeQuestionsTemplate):
+class Homepage(HomepageTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
 
     # Any code you write here will run before the form opens.
 
-  def goal_radio_other_clicked(self, **event_args):
-    """This method is called when this radio button is selected"""
-    self.goal_other.visible = self.goal_radio_other_clicked
-
-  def create_account_button_click(self, **event_args):
+  def outlined_button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    user =  anvil.users.login_with_form()
-    open_form('Homepage')
-
+    anvil.users.logout()
