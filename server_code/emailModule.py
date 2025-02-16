813bd9da3fef71eff88e1f5ce252ed0bf79008d9
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-02-16 03:44:04 +0000

    Edited settings, serverExperiments, openAiModule and emailModule

--- server_code/emailModule.py
+++ server_code/emailModule.py
@@ -5,7 +5,6 @@ import anvil.users
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
-import anvil.email
 import anvil.server
 from datetime import datetime
 
