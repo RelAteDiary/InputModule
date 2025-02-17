5ec1a1cad924e9185b9b1f624168b9ba08daed85
Liu <lutetium.helen@gmail.com>
2025-02-16 23:39:45 -0500

    Added some dummy test cases
    Also remove comments copied from example code

--- Tests/tests.py
+++ Tests/tests.py
@@ -1,19 +1,9 @@
-from unittest import TestCase
+import unittest
 
-# Server is required for the uplink connection
 import anvil.server
-
-# The user service can be useful to sign in and find database rows linked 
-# to the current user.
 import anvil.users
-
-# The tables can be used to check test results or to get the user row.
 from anvil.tables import app_tables
 
-# Different statements to import Globals when running on uplink vs on server.
-# Importing Globals will instantiate the global variable that will contain all 
-# the data used on the client.
-# For this example let's make "boxes" that global variable.
 if anvil.server.context.type == 'uplink':
     print('Running unit test locally...')
     import sys
@@ -26,8 +16,20 @@ else:
     # running on Anvil server
     from .Globals import *
 
-# This import allows the decorated server callable to run on the uplink.
-# Without it, they will run inside the app.
-# With this import the server callable will run in a thread different from the
-# test. Set a breakpoint on the server callable function to debug.
 import server_code.serverExperiments
+
+
+class TestMethods(unittest.TestCase):
+    def test_string_upper(self):
+        self.assertEqual('foo'.upper(), 'FOO')
+
+    def test_string_isupper(self):
+        self.assertTrue('FOO'.isupper())
+        self.assertFalse('Foo'.isupper())
+
+    def test_string_split(self):
+        s = 'hello world'
+        self.assertEqual(s.split(), ['hello', 'world'])
+
+    def test_math_multiplikation(self):
+        self.assertEqual(2 * 2, 5)
