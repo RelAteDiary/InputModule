008b12fb28e2ec7693b521eee4eb71fee61082d1
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-02-26 22:02:11 +0000

    Edited Diary.DiaryChip

--- client_code/Diary/DiaryChip.py
+++ client_code/Diary/DiaryChip.py
@@ -6,9 +6,16 @@ from m3.components import Button, TextBox, CardContentContainer, Checkbox
 class DiaryChip:
   def __init__(self):
     pass
+  def get_streak_tracker():
+    streak = anvil.server.call('diary_get_diary_streak')
+    if streak is None:
+      return None
+    if 'recent' in streak:
+      
+    
 
   def get_chip():
     panel = Card(appearance='filled',orientation='column')
     panel.add_component(RichText(content='# Diary'))
-    
+    panel.add_component(get_streak_tracker())
     return panel
