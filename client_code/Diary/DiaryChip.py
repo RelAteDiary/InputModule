cda8551f325f14856b23d75438d401a558512291
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-02-26 20:43:52 +0000

    Created HomePage, Diary.DiaryChip and Diary

---
+++ client_code/Diary/DiaryChip.py
@@ -0,0 +1,14 @@
+import anvil.server
+import anvil.google.auth, anvil.google.drive
+from anvil.google.drive import app_files
+import anvil.users
+import anvil.tables as tables
+import anvil.tables.query as q
+from anvil.tables import app_tables
+
+class DiaryChip:
+  def __init__(self):
+    pass
+
+  def get_chip():
+    
