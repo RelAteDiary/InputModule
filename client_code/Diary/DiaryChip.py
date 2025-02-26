fea87d0d21d6509daa6f3d378e62e9524e7ca8f3
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-02-26 21:05:26 +0000

    Edited Diary.DiaryChip

--- client_code/Diary/DiaryChip.py
+++ client_code/Diary/DiaryChip.py
@@ -2,10 +2,10 @@ import anvil.server
 from anvil import *
 from m3.components import Button, TextBox, CardContentContainer, Checkbox
 
+
 class DiaryChip:
   def __init__(self):
     pass
 
   def get_chip():
-    self.panel = 
-    
+    self.panel = None
