040d665097c0d5c8c69a6818c060bfc54221a8ef
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-02-26 20:45:27 +0000

    Edited Diary.DiaryChip

--- client_code/Diary/DiaryChip.py
+++ client_code/Diary/DiaryChip.py
@@ -1,14 +1,11 @@
 import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+from anvil import *
+from m3.components import Button, TextBox, CardContentContainer, Checkbox
 
 class DiaryChip:
   def __init__(self):
     pass
 
   def get_chip():
+    self.panel = 
     
