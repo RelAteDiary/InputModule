09d65d37516295b0f55d8cddf8f793468668bc7d
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-03-23 22:30:11 +0000

    Edited Diary.DiaryEntryForm.IngredientEditPopup

--- client_code/Diary/DiaryEntryForm/IngredientEditPopup/__init__.py
+++ client_code/Diary/DiaryEntryForm/IngredientEditPopup/__init__.py
@@ -1,17 +1,25 @@
 from ._anvil_designer import IngredientEditPopupTemplate
-from anvil import *
-import anvil.server
-import anvil.google.auth, anvil.google.drive
-from anvil.google.drive import app_files
-import anvil.users
-import anvil.tables as tables
-import anvil.tables.query as q
-from anvil.tables import app_tables
+from anvil import Label, TextBox
 
 
 class IngredientEditPopup(IngredientEditPopupTemplate):
-  def __init__(self, **properties):
-    # Set Form properties and Data Bindings.
+  def __init__(self, ingredient='', quantity=1, unit='', **properties):
+    self.ingredient = ingredient
+    self.quantity = quantity
+    self.unit = unit
+    
     self.init_components(**properties)
+    self.add_component(Label(text='The dish contains this ingredient:'))
+    ingredient_name_text_box = TextBox(text=ingredient)
+    self.add_component(ingredient_name_text_box)
+    ingredient_name_text_box.add_event_handler('lost_focus', self.set_ingredient)
 
-    # Any code you write here will run before the form opens.
+    self.add_component(Label('In this quantity:'))
+    quantity_panel = FlowPanel()
+    self.add_component()
+
+  def get_default_units(self):
+    return ['', 'tsp', 'tbsp', 'cups', 'oz', 'lb', 'g', 'piece','slice']
+
+  def set_ingredient(self,  **args):
+    self.ingredient = args['sender'].text
