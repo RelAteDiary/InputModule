5e9e7b6687f1af05e7e67c05c765040aeca9c4d5
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-03-26 21:07:18 +0000

    Edited settings and Diary.DiaryEntryForm.IngredientRowNew

--- client_code/Diary/DiaryEntryForm/IngredientRowNew/__init__.py
+++ client_code/Diary/DiaryEntryForm/IngredientRowNew/__init__.py
@@ -1,5 +1,5 @@
 from ._anvil_designer import IngredientRowNewTemplate
-from anvil import FlowPanel
+from anvil import FlowPanel, Label
 from m3.components import IconButton
 
 
@@ -9,29 +9,29 @@ class IngredientRowNew(IngredientRowNewTemplate):
     voilates_diets=[],
     amount="1",
     unit='serving',
-    ingredient="food",
+    ingredient_name="food",
     **properties,
   ):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    self.ingredient=ingredient
+    self.ingredient_name=ingredient_name
     self.amount = amount
     self.unit = unit
-    self.violates_diets = violates_diets
+    self.violates_diets = voilates_diets
 
     flow_panel = FlowPanel(gap="none", align="center")
     self.add_component(flow_panel)
 
     warn = IconButton(align="center", icon="warning", icon_color="red")
-    self.flow_panel_1.add_component(warn, width="15%")
-    label_1 = Label(text="blab blab", align="center")
-    self.flow_panel_1.add_component(label_1, width="27%", expand=True)
-    label_2 = Label(text="plep pleb", align="center")
-    self.flow_panel_1.add_component(label_2, width="27%", expand=True)
-    ed = IconButton(align="center")
-    self.flow_panel_1.add_component(ed, width="15%")
-    de = IconButton(align="center")
-    self.flow_panel_1.add_component(de, width="15%")
+    flow_panel.add_component(warn, width="15%")
+    amount_and_unit = Label(text=str(self.amount) + ' ' + self.unit, align="center")
+    flow_panel.add_component(amount_and_unit, width="27%", expand=True)
+    label_2 = Label(text=self.ingredient_name, align="center")
+    flow_panel.add_component(label_2, width="27%", expand=True)
+    edit = IconButton(icon='edit', align="center")
+    flow_panel.add_component(edit, width="15%")
+    delete = IconButton(icon='delete', align="center")
+    flow_panel.add_component(delete, width="15%")
 
   def set_violates_diets(self, diets):
     if len(diets) > 0:
