e9adf3873a3f255466cedebdce0252a331ec3bf0
lutetium.helen@gmail.com <lutetium.helen@gmail.com>
2025-04-04 18:48:58 +0000

    Edited Diary.IngredientDetails

--- client_code/Diary/IngredientDetails.py
+++ client_code/Diary/IngredientDetails.py
@@ -7,4 +7,5 @@ class IngredientDetails:
     self.amount = amount
     self.unit = unit
     self.amount_in_grams = amount_in_grams
+    # This field is not populated in the database
     self.violates_diets = violates_diets
